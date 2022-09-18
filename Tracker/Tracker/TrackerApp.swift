33403fa7c3f8013821e7cadc8fde21bc36a305bb
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-09-18 19:05:43 +0200

    Created AppDelegate

--- Tracker/Tracker/TrackerApp.swift
+++ Tracker/Tracker/TrackerApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct TrackerApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             EmptyView()
