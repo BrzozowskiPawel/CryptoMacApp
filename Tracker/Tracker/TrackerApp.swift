65aad2678b6fe96e2c0dd1c5bb18b4e8f1199271
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-09-18 18:57:43 +0200

    Created the project

---
+++ Tracker/Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Paweł Brzozowski on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
